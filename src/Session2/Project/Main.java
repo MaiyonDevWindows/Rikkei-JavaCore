476f3a7fb96c707419e3b67ea0a4c47d21d9a69d
MaiyonDevMacOSX <maiyonaisu1102@gmail.com>
2023-12-29 01:50:25 +0700

    create-Project2-Quan

--- src/Session2/Project/Main.java
+++ src/Session2/Project/Main.java
@@ -1,6 +1,5 @@
 package Session2.Project;
 
-import Session2.Project.Presentation.IManagement;
 import Session2.Project.PresentationImp.WarehouseManagement;
 
 import java.util.Scanner;
@@ -8,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        IManagement warehouseManagement = new WarehouseManagement();
-        warehouseManagement.displayMenu(scanner);
+        WarehouseManagement warehouseManagement = new WarehouseManagement();
+        warehouseManagement.displayAdminMenu(scanner);
     }
 }
